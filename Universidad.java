47abeadb78dd254cf12a5c119d0c8acbeaf2f938
gforero31934 <141641345+gforero31934@users.noreply.github.com>
2024-08-19 21:05:05 -0500

    Add files via upload

---
+++ Universidad.java
@@ -0,0 +1,55 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Universidad {
+
+    /**
+     * Default constructor
+     */
+    public Universidad() {
+    }
+
+    /**
+     * 
+     */
+    public Int Nit;
+
+    /**
+     * 
+     */
+    public String Nombre_Universidad;
+
+    /**
+     * 
+     */
+    public String Direccion_Universidad;
+
+    /**
+     * 
+     */
+    public String Nombre_Rector;
+
+    /**
+     * 
+     */
+    public String ID_Rector;
+
+    /**
+     * 
+     */
+    public void Agregar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Editar() {
+        // TODO implement here
+    }
+
+}
